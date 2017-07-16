faf19768d4a6fceac91b716737739161be08f4ac
Félix Pedrozo <pedrozofelix6991@gmail.com>
2017-07-16 17:17:59 -0400

    Se agregan los metodos 'putRutasDeArchivos' y 'getRutasDeArchivos'.

--- src/rabbit/io/ConfDeUsuario.java
+++ src/rabbit/io/ConfDeUsuario.java
@@ -19,6 +19,9 @@
 package rabbit.io;
 
 import rabbit.ui.EditorUI;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.prefs.Preferences;
 
 public class ConfDeUsuario {
@@ -62,5 +65,51 @@ public class ConfDeUsuario {
         String defaultValue = key.equals(KEY_TEMA) ? "intelli-j" : null;
 
         return prefs.get(key, defaultValue);
+    }
+
+    public static void putRutasDeArchivos (List <String> listArchivosRuta) {
+        String ruta = System.getProperty("user.home") + "/.rabbit";
+        File file = new File(ruta);
+
+        if (!file.exists()) file.mkdir();
+
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(ruta + "/file.list");
+
+            for (int i = 0; i < listArchivosRuta.size(); i ++) {
+                fileWriter.write(listArchivosRuta.get(i) + (listArchivosRuta.size() - 1 == i ? "" : "\n"));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fileWriter != null) fileWriter.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static List<String> getRutasDeArchivos () {
+        String ruta = System.getProperty("user.home") + "/.rabbit/file.list";
+        List<String> list = new ArrayList<>();
+        File file = new File (ruta);
+
+        if (file.exists()) {
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+
+                while ((ruta = reader.readLine()) != null)
+                    list.add(ruta);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return list;
     }
 }
